a923cda04c98d7ceda2544ec09bf93fd616bc5e0
jas.santos.987@gmail.com <jas.santos.987@gmail.com>
2018-09-02 23:40:33 +0000

    v6 authentication routes completed

--- YelpCamp/v6/app.js
+++ YelpCamp/v6/app.js
@@ -150,6 +150,19 @@ app.post("/register", function(req, res){
 });
 
 
+// SHOW LOGIN FORM
+app.get("/login", function(req, res){
+    res.render("login");
+});
+// HANDLING LOGIN LOGIC
+// Passport Middleware
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect:"/campgrounds",
+        failureRedirect:"/login"
+    }), function(req, res){
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!");
